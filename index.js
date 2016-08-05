1120961038e61fd10f76edc055336ff8ea3bf2a9
Sergey Tatarintsev <sergey.tatarintsev@researchgate.net>
2016-08-05 12:38:31 +0200

    All filepaths are now relative to system.projectRoot

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 'use strict';
+const path = require('path');
 const wrap = require('./lib/server-suite-wrapper');
 const Server = require('./lib/server');
 
 module.exports = function(gemini, options) {
-    const server = new Server(options);
+    const server = new Server({
+        projectRoot: gemini.config.system.projectRoot,
+        webpackConfig: path.resolve(gemini.config.system.projectRoot, options.webpackConfig)
+    });
+
     let geminiReact = null;
 
     Object.defineProperty(global, 'geminiReact', {
