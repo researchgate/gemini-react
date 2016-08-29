1c7f6ff49ed6e38a58d512013c4666fd3fb859e6
Sergey Tatarintsev <sergey.tatarintsev@researchgate.net>
2016-08-29 14:36:08 +0200

    Implement ability to load css
    There are two ways of loading styles:
    
    1. Stylesheets, that are always present can be loaded via cssFiles
    option.
    
    2. Test-specific stylesheets can be loaded with suite.includeCss option.
    
    Both options require staticRoot option to be set. This option should
    specify directory, in which the static assets are stored. It will be
    mounted to http server automatically.

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 'use strict';
 const path = require('path');
+const _ = require('lodash');
 const wrap = require('./lib/server-suite-wrapper');
 const Server = require('./lib/server');
 
 module.exports = function(gemini, options) {
+    const projectRoot = gemini.config.system.projectRoot;
+    if (_.size(options.cssFiles) > 0 && !options.staticRoot) {
+        throw new Error('staticRoot option is required when using cssFiles');
+    }
     const server = new Server({
-        projectRoot: gemini.config.system.projectRoot,
+        projectRoot: projectRoot,
         port: options.port || 5432,
         hostname: options.hostname || '127.0.0.1',
-        webpackConfig: path.resolve(gemini.config.system.projectRoot, options.webpackConfig)
+        staticRoot: options.staticRoot ? path.resolve(projectRoot, options.staticRoot) : null,
+        cssFiles: options.cssFiles || [],
+        webpackConfig: path.resolve(projectRoot, options.webpackConfig)
     });
 
     gemini.on('startRunner', () => {
