c0d11baa1c4223121a14ee5154060324f6fd8bdf
Sergey Tatarintsev <sergey.tatarintsev@researchgate.net>
2016-09-12 14:43:58 +0200

    Add separate server binary

--- index.js
+++ index.js
@@ -1,37 +1,14 @@
 'use strict';
-const path = require('path');
-const _ = require('lodash');
-const wrap = require('./lib/server-suite-wrapper');
-const Server = require('./lib/server');
+const setup = require('./lib/setup');
 
-module.exports = function(gemini, options) {
-    const projectRoot = gemini.config.system.projectRoot;
-    if (_.size(options.cssFiles) > 0 && !options.staticRoot) {
-        throw new Error('staticRoot option is required when using cssFiles');
-    }
-    const server = new Server({
-        projectRoot: projectRoot,
-        port: options.port || 5432,
-        hostname: options.hostname || '127.0.0.1',
-        staticRoot: options.staticRoot ? path.resolve(projectRoot, options.staticRoot) : null,
-        cssFiles: options.cssFiles || [],
-        webpackConfig: path.resolve(projectRoot, options.webpackConfig)
-    });
+module.exports = function(gemini) {
+    const server = setup(gemini);
 
     gemini.on('startRunner', () => {
         return server.start()
             .then(url => {
                 setRootUrl(gemini.config, url);
             });
-    });
-
-    gemini.on('beforeFileRead', (fileName) => {
-        global.geminiReact = wrap(global.gemini, server);
-        server.bundleTestFileForBrowser(fileName);
-    });
-
-    gemini.on('afterFileRead', () => {
-        delete global.geminiReact;
     });
 
     gemini.on('endRunner', () => server.stop());
